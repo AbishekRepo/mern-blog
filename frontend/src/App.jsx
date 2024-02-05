2402eda1cfc3ef04cdf39b3809c3f745ce2e4015
Abishek <abishekhttps@gmail.com>
2024-02-05 18:43:42 +0530

    FrontEnd: pages and routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,40 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/projects",
+    element: <Projects />,
+  },
+  {
+    path: "/sign-in",
+    element: <SignIn />,
+  },
+  {
+    path: "/sign-up",
+    element: <SignUp />,
+  },
+  {
+    path: "/Dashboard",
+    element: <Dashboard />,
+  },
+]);
+
 function App() {
-  return <div className="text-3xl">hi</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
